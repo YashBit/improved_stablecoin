0dba414d90835ca886839229665045f4e0ed0f6f
Yash Bharti <yashbharti@Yashs-MacBook-Pro.local>
2024-09-07 22:57:26 +0530

    feat: basic skeleton added

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 
 import { DecentralizedStableCoin } from "./DecentralizedStableCoin.sol";
-import { ReentrancyGuard } from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import { ReentrancyGuard } from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 /**
  * @title DSCEngine
  * @author Yash Bharti
@@ -11,7 +11,7 @@ import { ReentrancyGuard } from "@openzeppelin/contracts/security/ReentrancyGuar
  * @notice This contract is based on the MakerDAO DSS system
  * @dev The System is Designed to be as Minimal as Possible, and have the tokens maintain $1 == $1 Peg
  */
-contract DSCEngine {
+contract DSCEngine is ReentrancyGuard {
     error DSCEngine_CollateralMustBeGreaterThanZero();
     error DSCEngine__ArrayLengthMismatch();
     error DSCEngine__TokenNotAllowed(address token);
@@ -38,7 +38,8 @@ contract DSCEngine {
      * @param tokenAddress An array of token addresses that can be used as collateral
      * @param priceFeedAddress An array of price feed addresses corresponding to the token addresses
      * @param dscAddress The address of the DecentralizedStableCoin contract
-     */
+    */
+
     constructor(address[] memory tokenAddress, address[] memory priceFeedAddress, address dscAddress) {
         if (tokenAddress.length != priceFeedAddress.length) {
             revert DSCEngine__ArrayLengthMismatch();
@@ -55,11 +56,7 @@ contract DSCEngine {
      */
     function depositCollateralAndMintDsc() external {}
 
-    /**
-     * @notice Deposits collateral into the system
-     * @param tokenCollateral The address of the token to be used as collateral
-     * @param amount The amount of collateral to deposit
-     */
+
     function depositeCollateral(address tokenCollateralAddress , uint256 amount) external moreThanZero(amount) isAllowedToken(tokenCollateralAddress) nonReentrant  {}
 
     /**
